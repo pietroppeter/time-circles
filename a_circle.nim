f7cd20138e3fec9180da0db4511612eb6853e58d
Pietro Peterlongo <pietro.peterlongo@agilelab.com>
2024-11-09 12:40:19 -0500

    a circle is there

---
+++ a_circle.nim
@@ -0,0 +1,25 @@
+import nimib, p5
+nbInit
+nbUseP5
+
+nbCodeDisplay(nbJsFromCode):
+  const
+    lightBlue = "#02A4BD"
+    orange = "#ee961a"
+    sizeCanvas = 400
+    midCanvas = sizeCanvas / 2
+    sizeCircle = 30
+
+  func circle(x, y: float, size: float) =
+    # have not wrapped yet the circle function in p5nim
+    ellipse(x, y, size, size)
+
+  setup:
+
+    createCanvas(sizeCanvas, sizeCanvas)
+    background(lightBlue) # light blue
+    noStroke()
+    fill(orange)
+    circle(midCanvas, midCanvas, sizeCircle)
+
+nbSave
