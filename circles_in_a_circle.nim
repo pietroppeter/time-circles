c1b891b9c77dbc6466f0f6840c19b051b9d80f40
Pietro Peterlongo <pietro.peterlongo@agilelab.com>
2024-11-09 12:53:04 -0500

    two circles in a circle

---
+++ circles_in_a_circle.nim
@@ -0,0 +1,34 @@
+import nimib, p5
+nbInit
+nbUseP5
+
+nbCodeDisplay(nbJsFromCode):
+  const
+    lightBlue = "#02A4BD"
+    orange = "#ee961a"
+    aColor = "white"
+    sizeCanvas = 400
+    midCanvas = sizeCanvas / 2
+    sizeCircle = 30
+
+  proc circle(x, y: float, size: float) =
+    # have not wrapped yet the circle function in p5nim
+    ellipse(x, y, size, size)
+
+  proc bigCircle2(x, y: float) =
+    fill(aColor)
+    circle(x, y, sizeCircle*3)
+    fill(orange)
+    let deltaX = sizeCircle*0.6
+    circle(x -  deltaX, y, sizeCircle)    
+    circle(x +  deltaX, y, sizeCircle)    
+
+  setup:
+
+    createCanvas(sizeCanvas, sizeCanvas)
+    background(lightBlue) # light blue
+    noStroke()
+    fill(orange)
+    bigCircle2(midCanvas, midCanvas)
+
+nbSave
